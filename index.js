8b004a28f8f7ee13da4ddaea6af3e41317fe9a8e
Jerry Velazquez <jvr572@gmail.com>
2019-01-27 19:38:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ function add(num1, num2)
 {
   var sum = num1 + num2;
   return sum;
+}
+
+function subtract(num1, num2)
+{
+  var sum = num1 - num2;
+  return sum;
 }function add(num1, num2)
 {
   var sum = num1 + num2;
+  return sum;
+}
+
+function subtract(num1, num2)
+{
+  var sum = num1 - num2;
   return sum;
 }
